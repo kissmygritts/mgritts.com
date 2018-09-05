a685f23fc70cd0d9e2582d028b685d1a86f17c38
kissmygritts <kissmygritts08@gmail.com>
2018-09-04 17:35:05 -0700

    articles list styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,43 +9,48 @@ const IndexPage = ({ data }) => (
     <Hero />
 
     <div className="flex flex-column items-center">
-      <div className="avenir w-90 w-60-l">
+      <div className="avenir w-90 w-80-m w-two-thirds-l">
         <h3 className="f3 fw5">Articles</h3>
 
         {/* post list */}
         <div className="flex flex-column">
           {data.allMarkdownRemark.edges.map(({ node }) => (
-            <div key={node.id} className="flex flex-row pb2 f4">
-              {/* post */}
-              <div className="mr4 silver">{node.frontmatter.date}</div>
-              <div>
-                <Link
-                  to={node.fields.slug}
-                  className="no-underline hover-green underline-hover dark-gray"
-                >
+            <Link to={ node.fields.slug }>
+              <div
+                key={node.id}
+                className="flex flex-row justify-between flex-wrap pb2 f4 mid-gray"
+              >
+                <div className="w-100 w-two-thirds-l">
                   {node.frontmatter.title}
-                </Link>
+                </div>
+                <div className="silver">
+                  <small>{node.frontmatter.date}</small>
+                </div>
               </div>
-            </div>
+            </Link>
           ))}
         </div>
 
         {/* About */}
         <h3 className="f3 fw5 mt4">About</h3>
         <p>
-          I'm a wildlife ecologist and all around data nerd. I work as a data manager and web developer
-          at the Nevada Department of Wildlife. Lately, I've been working on designing fullstack data 
-          management systems to make our biologists jobs easier.
+          I'm a wildlife ecologist and all around data nerd. I work as a data
+          manager and web developer at the Nevada Department of Wildlife.
+          Lately, I've been working on designing fullstack data management
+          systems to make our biologists jobs easier.
         </p>
         <h4 className="f4">Open Lab Notebook</h4>
         <p>
-          After reading Carl Boettiger's lab notebooks and OpenWetWare I've decided to use this site as 
-          my "lab notebook". I'm a huge fan of open source, and in an effort to contribute to the open source 
-          community I will share my work as best I can on this site.
+          After reading Carl Boettiger's lab notebooks and OpenWetWare I've
+          decided to use this site as my "lab notebook". I'm a huge fan of open
+          source, and in an effort to contribute to the open source community I
+          will share my work as best I can on this site.
         </p>
 
         <p className="mv5 i">
-          Obligatory disclaimer: content herein are solely my own opinions and do not reflect the official policies of the Nevada Department of Wildlife, or the Government of the State of Nevada.
+          Obligatory disclaimer: content herein are solely my own opinions and
+          do not reflect the official policies of the Nevada Department of
+          Wildlife, or the Government of the State of Nevada.
         </p>
       </div>
     </div>
@@ -60,7 +65,7 @@ export const query = graphql`
         node {
           frontmatter {
             title
-            date
+            date(formatString: "MMM DD, YYYY")
           }
           id
           excerpt
